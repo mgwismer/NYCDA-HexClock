5310d18948ed67eef1c1eb35eea7ff6566e86c32
Margaret Wismer <margaretgwismer.gmail.com>
2016-11-17 16:41:19 -0500

    Trying to determine whether colors change

--- hexClock.js
+++ hexClock.js
@@ -11,8 +11,11 @@ function getTime() {
     document.getElementById('txt').innerHTML =
     h + ":" + m + ":" + s;
     var body = document.getElementById("body-class");
-    console.log("#"+h+m+s);
-    body.style.backgroundColor = "#"+s+m+h;
+    h1 = h*4;
+    s1 = s*4;
+    m1 = m*4;
+    console.log("#"+h1+m1+s1);
+    body.style.backgroundColor = "#"+h+m+s;
   }
 
 function checkTime(i) {
