a43b62bd49a7130c45dfefbcbea1cf0ffda9415b
Margaret Wismer <margaretgwismer.gmail.com>
2016-12-04 12:07:51 -0500

    greetings in array

--- hexClock.js
+++ hexClock.js
@@ -54,32 +54,6 @@ function changeTheSiteHourly(hour) {
     message.innerHTML = greetings[hour];
 }
 
-// function changeTheSiteHourly(hour) {
-//     var message = document.getElementById("clck-message");
-//     console.log(hour)
-//     if ((hour >= 7) && (hour <= 9)) {
-//     	message.innerHTML = "Good Morning, Time to Rise and Shine";
-//         document.body.style.backgroundImage = "url('images/lake.jpg')";
-//     }
-//     else if ((hour >= 10) && (hour <= 12)) {
-//     	message.innerHTML = "Hang on, almost time for lunch";
-//         document.body.style.backgroundImage = "url('images/lateMorning.jpg')";
-//     }
-//     else if ((hour >= 13) && (hour <= 15)){
-//     	message.innerHTML = "Ahhh, Enjoy Siesta";
-//         document.body.style.backgroundImage = "url('images/siesta.jpg')";
-//     }
-//     else if ((hour >= 15) && (hour <= 18)) {
-//     	message.innerHTML = "Tea time in Europe, Happy Hour?"
-//         document.body.style.backgroundImage = "url('images/mountain.jpg')";
-//     }
-//     else
-//     {
-//     	message.innerHTML = "Get a good's night rest!"
-//         document.body.style.backgroundImage = "url('images/winterNight.jpg')";
-//     }
-// }
-
 function checkTime(i) {
     if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
     return i;
